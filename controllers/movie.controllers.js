940dbe9ca9053fde1815fb068588511e7ec73418
Zain Ali <zainaliworkmail3830@gmail.com>
2025-10-02 00:38:34 -0700

    remove unnecessary console

--- controllers/movie.controllers.js
+++ controllers/movie.controllers.js
@@ -3,7 +3,6 @@ export const createMovie = async (req, res) => {
     try {
         const {title,duration, rating, release_date,genre,description,director,writers} = req.body;
         const picurl = req.files.image[0].path;
-        console.log(picurl);
         const newMovie = await pool.query('INSERT INTO movies (title,duration, rating, release_date,genre,description,director,writers,picurl) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *',[title,duration, rating, release_date,genre,description,director,writers,picurl]);
         res.json(newMovie.rows[0]);
     } catch (error) {
@@ -18,7 +17,6 @@ export const createMovie = async (req, res) => {
 export const getAllMovies = async (req,res)=>{
     try {
         const getMovies= await pool.query("SELECT * from movies")
-        console.log(getMovies.rows[0])
         res.json(getMovies.rows)
     } catch (error) {
         res.status(500).json({message:error.message})
@@ -32,7 +30,6 @@ export const getMovieById = async (req,res)=>{
     try {
         const {id} = req.params;
         const getMovieById= await pool.query("SELECT * from movies WHERE id = $1",[id])
-        console.log(getMovieById.rows[0])
         res.json(getMovieById.rows[0])
     } catch (error) {
         res.status(500).json({message:error.message})
